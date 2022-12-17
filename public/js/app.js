2e62f4ecee0d7630ab86f74bbaf3ec7d34eb1dd7
Guilherme <gsfrds1999@gmail.com>
2022-12-17 20:08:25 -0300

    Replaced some dummy text with a correct text

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    messageOne.textContent = 'Loading...'
+    messageOne.textContent = 'Just loading'
     messageTwo.textContent = ''
 
     
